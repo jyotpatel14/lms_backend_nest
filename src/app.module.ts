f83c11dd711102eefc875c201c23093b5dd4ca4c
jyotpatel14 <jyotkpatel14@gmail.com>
2025-05-15 15:28:11 +0530

    initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
